c6c5e968db49848cd1262feeec2f2fd39e1b8953
Thomas <Thomas@TPC>
2014-12-04 00:17:14 +0100

    added some tests

--- unidate/src/java/test/ImageTest.java
+++ unidate/src/java/test/ImageTest.java
@@ -5,10 +5,56 @@
  */
 package test;
 
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 /**
  *
  * @author Thomas
  */
 public class ImageTest {
-    
+
+
+    private Collection collection;
+ 
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        // one-time initialization code   
+    	System.out.println("@BeforeClass - oneTimeSetUp");
+    }
+ 
+    @AfterClass
+    public static void oneTimeTearDown() {
+        // one-time cleanup code
+    	System.out.println("@AfterClass - oneTimeTearDown");
+    }
+ 
+    @Before
+    public void setUp() {
+        collection = new ArrayList();
+        System.out.println("@Before - setUp");
+    }
+ 
+    @After
+    public void tearDown() {
+        collection.clear();
+        System.out.println("@After - tearDown");
+    }
+ 
+    @Test
+    public void testEmptyCollection() {
+        assertTrue(collection.isEmpty());
+        System.out.println("@Test - testEmptyCollection");
+    }
+ 
+    @Test
+    public void testOneItemCollection() {
+        collection.add("itemA");
+        assertEquals(1, collection.size());
+        System.out.println("@Test - testOneItemCollection");
+    }
 }
