57da75fb630387c5dc1e56cad980cc8ed2ab7a00
Jonas Heitz <heitzjon@students.zhaw.ch>
2014-12-03 11:08:26 +0100

    update insertForDatabase.sql

--- unidate/web/sql/insertsForDatabase.sql
+++ unidate/web/sql/insertsForDatabase.sql
@@ -1,4 +1,10 @@
 USE `unidate` ;
 
-INSERT INTO `student`(`firstname`, `lastname`, `username`, `email`, `password`, `age`, `interests`, `registrated`, `completedProfile`, `about`, `department`, `studium`) 
-VALUES ('Brianna', 'Fraser', 'fraserui','briana@zhaw.ch', '123456' , 23, 'woman', true, true,'something interesting about me' ,'Gesundheit', 'Physio');
+INSERT INTO `student`(`firstname`, `lastname`, `username`, `email`, `password`, `age`, `gender`, `interests`, `registrated`, `minage`, `maxage`, `completedProfile`, `about`, `department`, `studium`) 
+VALUES ('Brianna', 'Fraser', 'fraserui','briana@zhaw.ch', '123456' , 23, 'women', 'men', true, 18, 30, true,'something interesting about me' ,'Gesundheit', 'Physio');
+
+INSERT INTO `student`(`firstname`, `lastname`, `username`, `email`, `password`, `age`, `gender`, `interests`, `registrated`, `minage`, `maxage`, `completedProfile`, `about`, `department`, `studium`) 
+VALUES ('Peter', 'Pan', 'asd','panpet@students.zhaw.ch', 'asd' , 24, 'men', 'woman', true, 18, 30, true,'something interesting about me' ,'Technikum', 'Physio');
+
+INSERT INTO `likedStudent` ( `studentid`, `likedstudentid` )
+VALUES (2,1);
